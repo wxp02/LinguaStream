93e6277ff0d3be81bdb70e9578220c45f5a4b998
Andrew <andrewarochukwu@gmail.com>
2024-05-13 19:24:14 -0400

    signin and login pages done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from "react";
 import LandingPage from "./pages/LandingPage";
 import HomePage from "./pages/HomePage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/home" element={<HomePage />} />
+        <Route path="/signup" element={<Signup/>} />
+        <Route path="/login" element={<Login/>} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/Signup.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Signup.css";
+import { Link } from "react-router-dom";
+import google from "../assets/google.png";
+export default function Signup() {
+  return (
+    <div className="root">
+      <div className="holder">
+        <div className="signup-form">
+          <h1>Sign Up</h1>
+          <form>
+            <input type="text" placeholder="Full Name" />
+            <br />
+            <input type="text" placeholder="Email Address" />
+            <br />
+            <input type="password" placeholder="Password" />
+            <br />
+            <input type="password" placeholder="Confirm Password" />
+            <br />
+          </form>
+        </div>
+        <div className="other-side">
+          <button className="signup-button">Sign Up</button>
+          <p>
+            Already have an account?{" "}
+            <Link className="login-sign" to="/login">
+              Log in
+            </Link>
+          </p>
+          <p>Or</p>
+          <button className="google-button">
+            <img src={google} alt="Logo" />
+            Sign up with Google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
